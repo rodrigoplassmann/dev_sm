6ab215e2b6991773c84dde2b5f242d7f8338294d
Rodrigo Plassmann <rodrigo.plassmann@hotmail.com>
2026-08-08 17:37:36 -0300

    atualizacao rota de listar usuarios

--- dev_sm/routers/users.py
+++ dev_sm/routers/users.py
@@ -26,31 +26,4 @@ def create_user(user: UserSchema):
     response_model=UserListPublicSchema,
 )
 def list_users():
-    return {
-        'users': [
-            {
-                'id': 1,
-                'username': 'joao',
-                'email': 'joao@gmail.com',
-                'github_profile_url': 'https://github.com/joao',
-                'linkedin_profile_url': 'https://www.linkedin.com/in/joao',
-            },
-            {
-                'id': 2,
-                'username': 'jose',
-                'email': 'jose@gmail.com',
-                'github_profile_url': 'https://github.com/jose',
-            },
-            {
-                'id': 3,
-                'username': 'andre',
-                'email': 'andre@hotmail.com',
-                'linkedin_profile_url': 'https://www.linkedin.com/in/andre',
-            },
-            {
-                'id': 4,
-                'username': 'pedro',
-                'email': 'pedro@outlook.com',
-            },
-        ]
-    }
+    return {'users': USERS}
