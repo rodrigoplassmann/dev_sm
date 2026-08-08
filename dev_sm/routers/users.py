d2b804494eaa8a23731dc5cb0685863f647c0a82
Rodrigo Plassmann <rodrigo.plassmann@hotmail.com>
2026-08-08 15:58:01 -0300

    Rota para criar usuarios

--- dev_sm/routers/users.py
+++ dev_sm/routers/users.py
@@ -1,8 +1,23 @@
 from fastapi import APIRouter, status
-
-from dev_sm.schemas.users import UserListPublicSchema
+from dev_sm.db import USERS
+from dev_sm.schemas.users import (
+    UserSchema,
+    UserListPublicSchema,
+    UserPublicSchema,
+)
 
 router = APIRouter()
+
+
+@router.post(
+    path='/',
+    status_code=status.HTTP_201_CREATED,
+    response_model=UserPublicSchema,
+)
+def create_user(user: UserSchema):
+    user_with_id = UserPublicSchema(**user.model_dump(), id =len(USERS) + 1)
+    USERS.append(user_with_id)
+    return user_with_id
 
 
 @router.get(
