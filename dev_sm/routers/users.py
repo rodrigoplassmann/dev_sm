6e9f34e3de766edc16bc4e8564c919f61d4fa8bc
Rodrigo Plassmann <rodrigo.plassmann@hotmail.com>
2026-08-17 17:05:20 -0300

    Rota para atualizar usuario

--- dev_sm/routers/users.py
+++ dev_sm/routers/users.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, status
+
 from dev_sm.db import USERS
 from dev_sm.schemas.users import (
-    UserSchema,
     UserListPublicSchema,
     UserPublicSchema,
+    UserSchema,
 )
 
 router = APIRouter()
@@ -15,7 +16,7 @@ router = APIRouter()
     response_model=UserPublicSchema,
 )
 def create_user(user: UserSchema):
-    user_with_id = UserPublicSchema(**user.model_dump(), id =len(USERS) + 1)
+    user_with_id = UserPublicSchema(**user.model_dump(), id=len(USERS) + 1)
     USERS.append(user_with_id)
     return user_with_id
 
@@ -27,3 +28,14 @@ def create_user(user: UserSchema):
 )
 def list_users():
     return {'users': USERS}
+
+
+@router.put(
+    path='/{user_id}',
+    status_code=status.HTTP_201_CREATED,
+    response_model=UserPublicSchema,
+)
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserPublicSchema(**user.model_dump(), id=user_id)
+    USERS[user_id - 1] = user_with_id
+    return user_with_id
